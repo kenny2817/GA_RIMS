7fc277a0a2b9911d49d5b1c850c9ccfb9ecec119
kenny2817 <kenny2817.qm@gmail.com>
2025-04-01 10:48:07 +0200

    500 trc -> more resources

--- cluster/parsing_results.py
+++ cluster/parsing_results.py
@@ -50,6 +50,7 @@ def plot_results(key: tuple[str, str, str], data: dict, file_name: str):
     plt.title(f"Optimization Results trc: {key[0]}, pop: {key[1]}, ftol: {key[2]}")
     plt.xlabel("Duration")
     plt.ylabel("Cost")
+    plt.tight_layout()
     plt.legend([legend_text], loc="upper right", fontsize=10, frameon=True)
     plt.savefig(file_name + ".png")
     plt.close()
@@ -57,7 +58,7 @@ def plot_results(key: tuple[str, str, str], data: dict, file_name: str):
 def plot_trc(data: dict, folder: str):
     considered_results = []
     for key, results in data.items():
-        if key[1] == 50 and key[2] == 2.5e-05:
+        if key[1] == 50 and key[2] == 2.5e-05 and key[0] != 500:
             considered_results.append((key[0], round(results["generations"]/results["count"], 2), round(results["time"]/results["count"], 2)))
 
     sorted_considered_results = sorted(considered_results, key=lambda p: p[0])
@@ -70,16 +71,17 @@ def plot_trc(data: dict, folder: str):
     plt.plot(trc, time, color="red", label="time [s]")
     plt.title(f"Analysis of pop: 50 ftol: 2.5e-05")
     plt.xlabel("traces")
-    plt.xticks(trc)
+    plt.xticks(trc, rotation=60)
     plt.grid(True)
+    plt.tight_layout()
     plt.legend()
     plt.savefig(folder + "res_trc_gen_time.png")
     plt.close()
 
-def plot_pop(data, folder: str):
+def plot_pop(data, folder: str, trc: int):
     considered_results = []
     for key, results in data.items():
-        if key[0] == 400 and key[2] == 2.5e-05:
+        if key[0] == trc and key[2] == 2.5e-05:
             considered_results.append((key[1], round(results["generations"]/results["count"], 2), round(results["time"]/results["count"], 2), results["results"]))
 
     sorted_considered_results = sorted(considered_results, key=lambda p: p[0])
@@ -95,8 +97,9 @@ def plot_pop(data, folder: str):
     plt.xlabel("pop size")
     plt.xticks(pop)
     plt.grid(True)
+    plt.tight_layout()
     plt.legend()
-    plt.savefig(folder + "res_pop_gen_time.png")
+    plt.savefig(folder + f"res_pop_gen_time_{trc}.png")
     plt.close()
 
     plt.figure(figsize=(12, 9))
@@ -107,8 +110,9 @@ def plot_pop(data, folder: str):
     plt.xlabel("Duration")
     plt.ylabel("Cost")
     plt.grid(True)
+    plt.tight_layout()
     plt.legend()
-    plt.savefig(folder + "res_pop_res.png")
+    plt.savefig(folder + f"res_pop_res_{trc}.png")
     plt.close()
 
 def plot_ftol(data, folder: str):
@@ -122,6 +126,7 @@ def plot_ftol(data, folder: str):
     colors = [(1, 0, 0, alpha) for alpha in np.linspace(0.3, 1, len(considered_results))]
     # colors = ['yellow', 'orange', 'red', 'green', 'blue', 'purple', 'black']
     ftol, gen, time, res = zip(*sorted_considered_results)
+    tick_labels = [f"{x:.1e}" for x in ftol] 
 
     plt.figure(figsize=(12, 9))
     plt.yscale('log')
@@ -130,7 +135,8 @@ def plot_ftol(data, folder: str):
     plt.plot(ftol, time, color="red", label="time [s]")
     plt.title(f"Analysis of trc: 400 pop: 50")
     plt.xlabel("ftol")
-    plt.xticks(ftol)
+    plt.xticks(ftol, tick_labels, rotation=45, ha='right')  # Set ticks and labels
+    plt.tight_layout()
     plt.grid(True)
     plt.legend()
     plt.savefig(folder + "res_ftol_gen_time.png")
@@ -144,12 +150,13 @@ def plot_ftol(data, folder: str):
     plt.xlabel("Duration")
     plt.ylabel("Cost")
     plt.grid(True)
+    plt.tight_layout()
     plt.legend()
     plt.savefig(folder + "res_ftol_res.png")
     plt.close()
 
 
-log_file = "ignored/sim_31.txt"
+log_file = "ignored/sim_1.txt"
 parsed_data = parse_log(log_file)
 
 folder = "ignored/parsed_res/"
@@ -158,5 +165,6 @@ for key, data in parsed_data.items():
     plot_results(key, data, f"{folder}parsed_data_{key}")
 
 plot_trc(parsed_data, folder)
-plot_pop(parsed_data, folder)
+plot_pop(parsed_data, folder, 400)
+plot_pop(parsed_data, folder, 500)
 plot_ftol(parsed_data, folder)
